d54b72b5aff95276f011a1d794b3b34b18697619
tutugodfrey <godfrey_tutu@yahoo.com>
2023-10-26 16:21:41 +0100

    Dynamically modify pushgateway port for gosscheck

--- setup.sh
+++ setup.sh
@@ -15,8 +15,12 @@ cp goss.timer /etc/systemd/system/
 systemctl daemon-reload
 systemctl start goss.timer
 
-kubectl apply -f pushgateway.yaml
-
 # kubectl apply -f goss-endpoint.yaml
 # kubectl apply -f goss-service.yaml
 # kubectl apply -f goss-service-monitor.yaml
+kubectl apply -f pushgateway.yaml
+sleep 5
+# Get NodePort for pushgateway
+# PG_NODE_PORT=$(kubectl get svc -n monitoring | grep pushgateway-lb | awk -F" " '{ print $5 }' | awk -F'/' '{ print $1 }' | awk -F':' '{ print $2 }' )
+PUSHGATEWAY_NODE_PORT=$(kubectl get svc -n monitoring | grep pushgateway-lb |  awk -F'/' '{ print $1 }' | awk -F':' '{ print $2 }')
+sed -i s"|PUSHGATEWAY_PORT|${PUSHGATEWAY_NODE_PORT}|" /usr/local/bin/gosscheck
